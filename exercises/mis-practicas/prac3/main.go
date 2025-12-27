aa55292a570274e967aa18d79052be1f84e32143
GustavoAAA <gustavodev72270@gmail.com>
2025-12-27 14:01:23 -0500

    pract3

---
+++ exercises/mis-practicas/prac3/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type nuevoTipo2 string
+
+const (
+	INGRESO  nuevoTipo2 = "IN"
+	SALIDA   nuevoTipo2 = "OUT"
+	DESCANSO nuevoTipo2 = "REST"
+	REUNION  nuevoTipo2 = "MEETING"
+)
+
+func proceso(proc nuevoTipo2) {
+	switch proc {
+	case INGRESO:
+		fmt.Println("usted esta ingresando")
+	case SALIDA:
+		fmt.Println("usted esta saliendo")
+	case DESCANSO:
+		fmt.Println("usted esta entrando a su descanso")
+	case REUNION:
+		fmt.Println("usted esta entrando a una reunión")
+	}
+}
+
+func main() {
+	type nuevoTipo string
+
+	var edad nuevoTipo = "15"
+	fmt.Println(edad)
+
+	proceso(INGRESO)
+	proceso(REUNION)
+	proceso(DESCANSO)
+	proceso(SALIDA)
+}
